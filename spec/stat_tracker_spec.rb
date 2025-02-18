c612909982e6b1f9b255d34bbb358a7a977a1670
Fatima Guzman <fguzman1230@gmail.com>
2025-02-18 10:29:57 -0700

    feat: Add StatTracker class with #initialize method code

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -15,23 +15,14 @@ RSpec.describe StatTracker do
     
     it 'has attributes of game_id, season, season_type, date_time, away_team_id, home_team_id, away_goals, home_goals, venue, venue_link' do
       expect(stat_tracker.game_id).to eq(2012030221)
-      
       expect(stat_tracker.season).to eq(20122013)
-      
       expect(stat_tracker.season_type).to eq("Postseason")
-      
       expect(stat_tracker.date_time).to eq(5/16/13)
-      
       expect(stat_tracker.away_team_id).to eq(3)
-      
       expect(stat_tracker.home_team_id).to eq(6)
-      
       expect(stat_tracker.away_goals).to eq(2)
-      
       expect(stat_tracker.home_goals).to eq(3)
-      
       expect(stat_tracker.venue).to eq("Toyota Stadium")
-      
       expect(stat_tracker.venue_link).to eq("/api/v1/venues/null")
     end
   end
